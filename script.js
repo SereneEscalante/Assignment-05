1829a3723ed51bdd5c8938dcf1d31141a0fbdac8
SereneEscalante <rahmanmah2003@gmail.com>
2026-03-09 20:26:56 +0600

    9th commit

--- script.js
+++ script.js
@@ -25,10 +25,34 @@ loginForm.addEventListener("submit", (e) => {
     if(user === "admin" && pass === "admin123") {
         loginPage.classList.add("hidden");
         dashboard.classList.remove("hidden");
+
         fetchIssues();
     } else {
         alert("Wrong Credentials");
     }
 });
 
+
 //fetching the issues
+async function fetchIssues() {
+    toggleLoader(true);
+
+    try {
+        const res = await fetch(`${API_BASE}/issues`);
+        const data = await res.json();
+
+        allIssues = data.data;
+
+        renderIssues(allIssues);
+    } catch (error) {
+        console.error("Fetch error:", error);
+
+        issuesGrid.innerHTML =
+        `<p class="text-red-500 text-center col-span-full">
+        Failed to load issues</p>`
+    } finally {
+        toggleLoader(false);
+    }
+}
+
+//render issue cards
